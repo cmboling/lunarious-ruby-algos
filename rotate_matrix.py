c1e3aa468a6667da1310d50de5fd092a4d9425db
cmboling <cmboling@me.com>
2019-12-10 16:35:56 -0800

    question that I failed today but didn't fail in college T_T

---
+++ rotate_matrix.py
@@ -0,0 +1,20 @@
+N = 1000
+
+def rotateoldMatrixrix(oldMatrix): 
+    for x in range(int(N/2)): 
+        for y in range(x, N-x-1): 
+              
+            # store temp 
+            temp = oldMatrix[x][y] 
+  
+            # right to top 
+            oldMatrix[x][y] = oldMatrix[y][N-1-x] 
+  
+            # bottom to right 
+            oldMatrix[y][N-1-x] = oldMatrix[N-1-x][N-1-y] 
+  
+            # left to bottom 
+            oldMatrix[N-1-x][N-1-y] = oldMatrix[N-1-y][x] 
+  
+            # assign temp
+            oldMatrix[N-1-y][x] = temp 
